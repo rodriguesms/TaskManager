bee5e7779ef3fc2c988e96c14c7bff46095ced16
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2022-09-03 19:54:26 -0300

    Init

---
+++ backend/demo/src/main/java/com/dimastec/demo/DimasChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dimastec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimasChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimasChallengeApplication.class, args);
+	}
+
+}
